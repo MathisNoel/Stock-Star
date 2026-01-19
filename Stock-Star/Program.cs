c40f50db2748aa6c57e394a894b2ca4f71c12ec0
obck <obuyukcetinkaya@gmail.com>
2026-01-19 17:41:48 +0100

    Changement du Nom du Project

--- Stock-Star/Program.cs
+++ Stock-Star/Program.cs
@@ -1,4 +1,4 @@
-namespace project_TESTC_sharp
+namespace Stock_Star
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace project_TESTC_sharp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new OBSM());
+            Application.Run(new Form1());
         }
     }
 }
