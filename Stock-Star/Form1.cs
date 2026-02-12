f79c21e468aef6d80556524ce830628cbe1c2ab7
MathisNoel <mathis.noel2021@gmail.com>
2026-02-12 19:08:02 +0100

    Création BDD
    Création de la BDD + Création de la table Produit.
    Et, Tentative de Connection de l'application C# a la BDD mais erreur

--- Stock-Star/Form1.cs
+++ Stock-Star/Form1.cs
@@ -2,6 +2,7 @@ namespace Stock_Star
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
--- Stock-Star/Program.cs
+++ Stock-Star/Program.cs
@@ -1,6 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+//On inclue la librairie
+using Npgsql;
+using NpgsqlTypes;
+//Fin
+using System.Linq;
+using System.Data;
+using System.Text;
+
+
 namespace Stock_Star
 {
-    internal static class Program
+    public class produits
+    {
+            //Connection a Npgsql
+            NpgsqlConnection myConnection = null;
+            string connString = "Server=localhost;Port=5432;Database=produits;User Id=user;Password=user;";
+            myConnection = new NpgsqlConnection(connString);
+            myConnection.open();
+    }
+
+        internal static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
