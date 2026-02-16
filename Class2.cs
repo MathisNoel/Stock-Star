f6e13984849e5dcac9fb76d9fa6ecc4f98628cc5
obck <obuyukcetinkaya@gmail.com>
2026-02-16 17:21:46 +0100

    création de la classe produit

---
+++ Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+    public class Produit
+    {
+        public : 
+         string Nom { get; set; }
+         decimal PrixAchat { get; set; }
+         decimal? PrixVente { get; set; }
+
+         decimal Taille { get; set; }
+         string Emplacement { get; set; }
+         string Description { get; set; }
+    }
+
